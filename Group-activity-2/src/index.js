7082df6c308c2e3270637e3a6bcde4fafa730e38
iirasalo <iira.salo@metropolia.fi>
2023-02-03 15:36:45 +0200

    Solutions for Group activity 2

--- Group-activity-2/src/index.js
+++ Group-activity-2/src/index.js
@@ -9,15 +9,28 @@ add.addEventListener('click', async () => {
     const url = data.message; // URL of new dog image
     console.log(url);
     /*--------------- Get breed (Hint: Parse from URL using split) ---------------- */
-    // Your code here
+    const splitURL = url.toString().split("/");
+    const breed = splitURL[4];
+    console.log(breed);
 
     /*------------ Create new dog card with the url above ------------- */
     /* (use the HTML structure for the current dog image in the index.html
             file to create a new image with the url) */
-    // Your code here
+    // 
+    const newListItem = document.createElement('li');
+    const figure = document.createElement('figure');
+    const image = document.createElement('img');
+    const figcaption = document.createElement('figcaption');
+    
+    image.setAttribute('src', `${url}`);
+    figcaption.innerText = `${breed}`;
+    newListItem.appendChild(figure);
+    figure.appendChild(image);
+    figure.appendChild(figcaption);
 
     /* Add the new dog card as a child to the ul in the .gallery element */
-    // Your code here
+    const list = document.querySelector('ul');
+    list.appendChild(newListItem);
   } catch (e) {
     console.log("Couldn't fetch dog :(");
   }
@@ -27,10 +40,15 @@ add.addEventListener('click', async () => {
 // const removeFirst = document.getElementById('remove-first');
 const removeFirst = document.querySelector('#remove-first');
 removeFirst.addEventListener('click', () => {
-  /*-------------------- Select the first dog card --------------------- */
-  // Your code here
-  /*-------------------- Remove the first dog card --------------------- */
-  // Your code here
+    /*-------------------- Select the first dog card --------------------- */
+    // Your code here
+    // const firstDog = document.getElementsByTagName('li')[0];
+    const firstDoggo = document.querySelector('li');
+    if (!firstDoggo) {
+      console.log('no dog in the list!');
+      return;
+    }
+    firstDoggo.remove();
 });
 
 /************************** REMOVE LAST DOG BUTTON ***************************/
@@ -38,7 +56,16 @@ removeFirst.addEventListener('click', () => {
 const removeLast = document.querySelector('#remove-last');
 removeLast.addEventListener('click', () => {
   /*-------------------- Select the last dog card ----------------------- */
-  // Your code here
+ 
+  const listOfDogs = document.querySelectorAll('li');
+  console.log(listOfDogs);
+  length = listOfDogs.length;
+  if (length === 0) {
+    console.log('no dog in the list!');
+    return;
+  }
+  const lastDog = listOfDogs[length - 1];
+
   /*-------------------- Remove the last dog card ----------------------- */
-  // Your code here
+  lastDog.remove();
 });

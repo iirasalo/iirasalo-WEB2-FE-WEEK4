a489a25c705b52e1d28cc55e3e5d5ff5c2956cbb
iirasalo <iira.salo@metropolia.fi>
2023-02-03 14:26:41 +0200

    Group activity 1, solutions

--- Group-activity-1/src/index.js
+++ Group-activity-1/src/index.js
@@ -1,1 +1,18 @@
-//Your code here
+fetch('https://xkcd.now.sh/?comic=latst')
+.then(response => response.json())
+.then(data => {
+ console.log('This is data: ', data);
+
+  const image = document.createElement('img');
+  image.setAttribute('src', data.img);
+  image.alt = data.alt;
+  const container = document.querySelector("#container");
+  container.appendChild(image);
+})
+.catch(error => {
+    if (error.status === 404) {
+      console.log('ERROR!')
+    }
+  });
+
+
